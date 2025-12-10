b1d4dc0834ebe56464622d646aba088679cdb9e1
ZandUY <2239368296@qq.com>
2025-12-10 10:45:48 +0800

    测试

--- src/code.oldboyedu.com/studygo/day09/http_server/main.go
+++ src/code.oldboyedu.com/studygo/day09/http_server/main.go
@@ -15,8 +15,14 @@ func f1(w http.ResponseWriter, r *http.Request) {
 
 	w.Write(page)
 }
+func f2(w http.ResponseWriter, r *http.Request) 
+{
+	fmt.Println(r.URL)
+	fmt.Println(r.Method)
+}
 func main() {
 	http.HandleFunc("/posts/Go/unit-test/", f1)
+	http.HandleFunc("/hello/",f2)
 	http.ListenAndServe("127.0.0.1:9090", nil)
 
 }
